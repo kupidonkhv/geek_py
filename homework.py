b06af22e979d85575127262a63b9152bf417d75b
kupidonkhv <dokar99@mail.ru>
2021-02-04 15:51:35 +1000

    homework #6

--- homework.py
+++ homework.py
@@ -1,123 +1,160 @@
 #1
-out_f = open("my_file_0001.txt", "w")
-inp = input("Введите новую строку. Для окончания введите пустую строку.")
-while inp != "":
-    out_f.write(f"{inp}\n")
-    inp = input("Введите новую строку. Для окончания введите пустую строку.")
+from time import sleep
 
-out_f.close()
+
+class SvetoFor:
+    __text = ['Красный', 'Желтый', 'Зеленый']
+    __color = ['\033[31m {}', '\033[33m {}', '\033[32m {}']
+
+    def runn(self):
+        i = 0
+        while i != 3:
+            print(SvetoFor.__color[i].format(SvetoFor.__text[i]))
+            if i == 0:
+                sleep(7)
+            elif i == 1:
+                sleep(2)
+            elif i == 2:
+                sleep(5)
+            i += 1
+
+
+while True:
+    t = SvetoFor()
+    t.runn()
 
 
 #2
-word = 0
-words = []
-for line in open("text_6.txt"):
-    pos = 'out'
-    for letter in line:
-        if letter != ' ' and pos == 'out':
-            word += 1
-            pos = 'in'
-        elif letter == ' ':
-            pos = 'out'
-    words.append(word)
-    word = 0
-print("Строк в указанном файле:", len(words))
-for i in range(len(words)):
-    print(f"В строке {i+1} - слов: {words[i]}")
+class Road:
+
+    def __init__(self, length, width):
+        self._length = length
+        self._width = width
+        self.weight = 25
+        self.height = 5
+
+    def asphalt_mass(self):
+        asphalt_mass = self._length * self._width * self.weight * self.height / 1000
+        print(f'масса асфальта для покрытия одного кв метра дороги асфальтом, толщиной в 1 см: {round(asphalt_mass)}')
+
+
+r = Road(5000, 20)
+r.asphalt_mass()
 
 
 #3
-file = open("text_3.txt", "r", encoding="utf-8")
-users = {}
-for line in file:
-    line = line.replace("\n", "")
-    line = line.split(" ")
-    users[line[0]] = float(line[1])
-file.close()
+class Worker:
 
-print("Зарплаты менее 20 тыс. получают:")
-for fio, oklad in users.items():
-    if oklad < 20000:
-        print(fio)
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": int(wage), "bonus": int(bonus)}
 
-print(f"Зарплатный фонд: {sum(users.values())}")
-print(f"Средняя зарплата на сотрудника ({len(users)} человек): {sum(users.values())/len(users)}")
+
+class Position(Worker):
+    def __init__(self, name, surname, position, wage, bonus):
+        super().__init__(name, surname, position, wage, bonus)
+
+    def get_full_name(self):
+        return self.name + ' ' + self.surname
+
+    def get_total_income(self):
+        return self._income["wage"] + self._income["bonus"]
+
+
+p = Position('Сергей', 'Алексейцев', 'Кодер', '100000', '100000')
+print(p.get_full_name(), p.get_total_income())
 
 
 #4
-from deep_translator import GoogleTranslator
-file = open("text_4.txt", "r", encoding="utf-8")
-my_list = ""
-for line in file:
-    line = line.replace('\n', '').split(" - ")
-    my_list = my_list + GoogleTranslator(source='auto', target='ru').translate(line[0]) + "\n"
-file.close()
+class Car:
 
-out_f = open("out_file.txt", "w")
-out_f.write(my_list)
-out_f.close()
+    def __init__(self, name, speed, color, is_police=False):
+        self.name = name
+        self.speed = speed
+        self.color = color
+        self.is_police = is_police
+
+    def go(self):
+        return f'Машина {self.name} поехала.'
+
+    def stop(self):
+        return f'\n{self.name} остановилась.'
+
+    def turn(self, direction):
+        return f'\nМашина {self.name} повернула {direction}'
+
+    def show_speed(self):
+        return f'\nСкорость машины: {self.speed}'
+
+
+class TownCar(Car):
+    def show_speed(self):
+        if self.speed > 60:
+            return f'\nПревышении скорости {self.speed}!'
+
+
+class SportCar(Car):
+    def show_speed(self):
+        if self.speed > 300:
+            return f'\nПревышении скорости {self.speed}!'
+
+
+class WorkCar(Car):
+    def show_speed(self):
+        if self.speed > 40:
+            return f'\nПревышении скорости: {self.speed}'
+
+
+class PoliceCar(Car):
+    def turn_on_flashing_light(self):
+        return f'\nИииииууу Иииииууу Иииииууу Иииииууу!'
+
+    def piu_piu(self):
+        return f'\nСтреляем по преследуему!'
+
+
+town = TownCar('Audi TT', 70, 'blue', False)
+print(town.go(), town.show_speed(), town.turn('направо'), town.turn('направо'), town.stop())
+
+sport = SportCar('Lamborghini Diablo', 170, 'синяя', False)
+print(sport.go(), sport.show_speed(), sport.turn('налево'), sport.stop())
+
+work = WorkCar('МАЗ', 30, 'красная', False)
+print(work.go(), work.show_speed(), work.turn('направо'), work.stop())
+
+police = PoliceCar('Бэтмобиль', 100, 'жёлтая', True)
+print(police.go(), police.turn_on_flashing_light(), police.show_speed(), police.turn('полицейский разворот'), police.go(), police.turn('направо'), police.piu_piu(), police.stop())
 
 
 #5
-with open('file_5.txt', 'w+', encoding="utf-8") as file_obj:
-    line = input('Введите цифры через пробел \n')
-    file_obj.writelines(line)
-    num = line.split()
-    print(sum(map(int, num)))
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        return f'Запуск отрисовки'
 
 
-#6
-subj = {}
-with open('text_6.txt', 'r', encoding="utf-8") as file:
-    for line in file:
-        lecture_int = practice_int = lab_int = 0
-
-        subject, lecture, practice, lab = line.split()
-
-        lecture_list = [int(num) for num in filter(lambda num: num.isnumeric(), lecture)]
-        practice_list = [int(num) for num in filter(lambda num: num.isnumeric(), practice)]
-        lab_list = [int(num) for num in filter(lambda num: num.isnumeric(), lab)]
-
-        if type(lecture_list) == list and lecture_list != []:
-            lecture_int = ''.join(str(e) for e in lecture_list)
-        else:
-            lecture_int = 0
-        if type(practice_list) == list and practice_list != []:
-            practice_int = ''.join(str(e) for e in practice_list)
-        else:
-            practice_int = 0
-        if type(lab_list) == list and lab_list != []:
-            lab_int = ''.join(str(e) for e in lab_list)
-        else:
-            lab_int = 0
-
-        print(f"Общее количество часов по {subject} {int(lecture_int) + int(practice_int) + int(lab_int)}")
+class Pen(Stationery):
+    def draw(self):
+        return f'Запуск отрисовки {self.title}'
 
 
-#7
-import json
+class Pencil(Stationery):
+    def draw(self):
+        return f'Запуск отрисовки {self.title}'
 
-profit = {}
-pr = {}
-prof = 0
-prof_aver = 0
-i = 0
-with open('text_7.txt', 'r+', encoding="utf-8") as file:
-    for line in file:
-        f_n, name, earning, damage = line.split()
-        profit[name] = int(earning) - int(damage)
-        if profit.setdefault(name) >= 0:
-            prof = prof + profit.setdefault(name)
-        i += 1
-        if i >= 0:
-            prof_aver = prof / i
-    else:
-        pr_sr = ({'Средняя прибыль компаний': round(prof_aver)})
-        profit.update(pr_sr)
-    print(f'Прибыль всех компании: - {prof:.2f}')
-    print(f'Средняя прибыль всех компаний: - {round(prof_aver):.2f}')
-    print(f'Сводные результаты компаний: - {profit}')
 
-with open('test_7.json', 'w+', encoding="utf-8") as write_js:
-    json.dump(profit, write_js)
-print(json.dumps(profit), ensure_ascii=False)
+class Handle(Stationery):
+    def draw(self):
+        return f'Запуск отрисовки {self.title}'
+
+
+pen = Pen('ручкой')
+print(pen.draw())
+pencil = Pencil('карандашем')
+print(pencil.draw())
+handle = Handle('маркером')
+print(handle.draw())
